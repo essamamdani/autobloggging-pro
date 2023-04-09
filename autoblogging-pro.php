bdf8a329a7c73475eff3ab82bbe26090f1d7af89
Essa Mamdani <mamdani.info@gmail.com>
2023-04-09 15:41:41 +0000

    Update

--- autoblogging-pro.php
+++ autoblogging-pro.php
@@ -18,35 +18,35 @@
 
 namespace AutoBlogging_Pro;
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
  * Root directory to this plugin.
  */
-define('AUTOBLOGGING_PRO_DIR', __DIR__);
-define('AUTOBLOGGING_PRO_URL', plugin_dir_url(__FILE__));
-define('AUTOBLOGGING_PRO_FILE', __FILE__);
-define('AUTOBLOGGING_PRO_VERSION', '1.0');
-define('AUTOBLOGGING_PRO_NAME', 'AutoBlogging Pro');
-define('AUTOBLOGGING_PRO_API_URL', 'https://app.autoblogging.pro/');
+define( 'AUTOBLOGGING_PRO_DIR', __DIR__ );
+define( 'AUTOBLOGGING_PRO_URL', plugin_dir_url( __FILE__ ) );
+define( 'AUTOBLOGGING_PRO_FILE', __FILE__ );
+define( 'AUTOBLOGGING_PRO_VERSION', '1.0' );
+define( 'AUTOBLOGGING_PRO_NAME', 'AutoBlogging Pro' );
+define( 'AUTOBLOGGING_PRO_API_URL', 'https://app.autoblogging.pro/' );
 
 // Check if Composer is installed (remove if Composer is not required for your plugin).
-if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	// Will also check for the presence of an already loaded Composer autoloader
 	// to see if the Composer dependencies have been installed in a parent
 	// folder. This is useful for when the plugin is loaded as a Composer
 	// dependency in a larger project.
-	if (!class_exists(\Composer\InstalledVersions::class)) {
+	if ( ! class_exists( \Composer\InstalledVersions::class ) ) {
 		\add_action(
 			'admin_notices',
 			function () {
-?>
+				?>
 			<div class="notice notice-error">
-				<p><?php esc_html_e('Composer is not installed and autoblogging-pro cannot load. Try using a `*-built` branch if the plugin is being loaded as a submodule.', 'autoblogging-pro'); ?></p>
+				<p><?php esc_html_e( 'Composer is not installed and autoblogging-pro cannot load. Try using a `*-built` branch if the plugin is being loaded as a submodule.', 'autoblogging-pro' ); ?></p>
 			</div>
-<?php
+				<?php
 			}
 		);
 
@@ -66,8 +66,7 @@ require_once __DIR__ . '/src/meta.php';
 /**
  * Instantiate the plugin.
  */
-function main(): void
-{
+function main(): void {
 	// ...
 	\AutoBlogging_Pro\AutoBlogging_Pro::instance();
 }
--- templates/settings.php
+++ templates/settings.php
@@ -3,30 +3,30 @@
 	<h1 class="text-center text-2xl mb-4">AutoBlogging Pro</h1>
 	<p class="text-center mb-4">Connect to your AutoBlogging Pro account to get started.</p>
 	<div class="flex justify-center">
-		<?php if (empty($api_key)) : ?>
-			<a href="<?php echo esc_url($connect_api) . '?redirect_to=' . base64_encode('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>" class="bg-indigo-600 text-white font-bold py-2 px-4 rounded">Connect</a>
+		<?php if ( empty( $api_key ) ) : ?>
+			<a href="<?php echo esc_url( $connect_api ) . '?redirect_to=' . base64_encode( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ); ?>" class="bg-indigo-600 text-white font-bold py-2 px-4 rounded">Connect</a>
 		<?php else : ?>
 			<div class="wrap">
-				<h1><?php esc_html_e('Autoblogging Pro Settings', 'autoblogging-pro'); ?></h1>
+				<h1><?php esc_html_e( 'Autoblogging Pro Settings', 'autoblogging-pro' ); ?></h1>
 
 				<div class="flex justify-between">
 					<!-- disconnect api key with form -->
-					<button class="btn bg-red-700 text-white font-bold py-2 px-4 rounded btn_action" data-action="autoblogging_pro_disconnect_api_key"><?php esc_html_e("Disconnect") ?></button>
+					<button class="btn bg-red-700 text-white font-bold py-2 px-4 rounded btn_action" data-action="autoblogging_pro_disconnect_api_key"><?php esc_html_e( 'Disconnect' ); ?></button>
 
-					<button class="btn bg-blue-700 text-white font-bold py-2 px-4 rounded btn_action" data-action="autoblogging_pro_fetch_now"><?php esc_html_e("Fetch now") ?></button>
+					<button class="btn bg-blue-700 text-white font-bold py-2 px-4 rounded btn_action" data-action="autoblogging_pro_fetch_now"><?php esc_html_e( 'Fetch now' ); ?></button>
 
 				</div>
 				<form method="post" action="options.php">
-					<?php settings_fields('autoblogging_pro_settings_group'); ?>
+					<?php settings_fields( 'autoblogging_pro_settings_group' ); ?>
 					<table class="form-table">
-						<?php do_settings_sections('autoblogging_pro_settings_group'); ?>
+						<?php do_settings_sections( 'autoblogging_pro_settings_group' ); ?>
 					</table>
 					<?php submit_button(); ?>
 				</form>
 				<script>
 					jQuery(document).ready(function($) {
 
-						<?php if ($action !== 'schedule') : ?>
+						<?php if ( $action !== 'schedule' ) : ?>
 							// Hide the Schedule Interval and Schedule Time fields on load
 							$('.autoblogging_pro_schedule_settings').hide();
 						<?php endif; ?>
@@ -45,7 +45,7 @@
 						$('.btn_action').click(function() {
 							var action = $(this).data('action');
 							$.ajax({
-								url: '<?php echo admin_url('admin-ajax.php'); ?>',
+								url: '<?php echo admin_url( 'admin-ajax.php' ); ?>',
 								type: 'POST',
 								data: {
 									action: action,
