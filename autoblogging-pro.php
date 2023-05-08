5d2f432c2cbbb4ccaab5bafd6656199cc8e7f303
Essa Mamdani <mamdani.info@gmail.com>
2023-05-08 22:12:44 +0500

    Removed Composer.json and Vendor Folder

--- autoblogging-pro.php
+++ autoblogging-pro.php
@@ -32,35 +32,8 @@ define( 'AUTOBLOGGING_PRO_VERSION', '1.0' );
 define( 'AUTOBLOGGING_PRO_NAME', 'AutoBlogging Pro' );
 define( 'AUTOBLOGGING_PRO_API_URL', 'https://app.autoblogging.pro/' );
 
-// Check if Composer is installed (remove if Composer is not required for your plugin).
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	// Will also check for the presence of an already loaded Composer autoloader
-	// to see if the Composer dependencies have been installed in a parent
-	// folder. This is useful for when the plugin is loaded as a Composer
-	// dependency in a larger project.
-	if ( ! class_exists( \Composer\InstalledVersions::class ) ) {
-		\add_action(
-			'admin_notices',
-			function () {
-				?>
-			<div class="notice notice-error">
-				<p><?php esc_html_e( 'Composer is not installed and autoblogging-pro cannot load. Try using a `*-built` branch if the plugin is being loaded as a submodule.', 'autoblogging-pro' ); ?></p>
-			</div>
-				<?php
-			}
-		);
-
-		return;
-	}
-} else {
-	// Load Composer dependencies.
-	require_once __DIR__ . '/vendor/autoload.php';
-}
-
 // Load the plugin's main files.
 require_once __DIR__ . '/src/meta.php';
-
-
 
 
 /**
--- vendor/alleyinteractive/wp-filter-side-effects/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-This library adheres to [Semantic Versioning](https://semver.org/) and [Keep a CHANGELOG](https://keepachangelog.com/en/1.0.0/).
-
-## 1.0.0
-
-Initial release.
